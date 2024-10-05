8e15cdf374197e6f6a3e4f77edd612e49d38421e
randythomas3 <randy.thomas3.9393@gmail.com>
2024-10-05 01:47:17 +0100

    Make the extension reload tabs within the current window only

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.action.onClicked.addListener(() => {
-    chrome.tabs.query({}, (tabs) => {
+    chrome.tabs.query({currentWindow: true}, (tabs) => {
       for (let tab of tabs) {
         chrome.tabs.reload(tab.id);
       }
